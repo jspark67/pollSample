88fb53b6e6611775b28fecd65c13aecb0cc86229
jspark67 <jspark67@gmail.com>
2012-11-14 01:07:04 +0900

    use generic code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,18 +5,18 @@ from django.core.urlresolvers import reverse
 
 from polls.models import Choice, Poll
 
-def index(request):
-  latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-  context = {'latest_poll_list': latest_poll_list}
-  return render(request, 'polls/index.html', context)
+# def index(request):
+#   latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#   context = {'latest_poll_list': latest_poll_list}
+#   return render(request, 'polls/index.html', context)
 
-def detail(request, poll_id):
-  poll = get_object_or_404(Poll, pk=poll_id)
-  return render(request, 'polls/detail.html', {'poll': poll})
+# def detail(request, poll_id):
+#   poll = get_object_or_404(Poll, pk=poll_id)
+#   return render(request, 'polls/detail.html', {'poll': poll})
 
-def results(request, poll_id):
-  poll = get_object_or_404(Poll, pk=poll_id)
-  return render(request, "polls/results.html", {'poll': poll})
+# def results(request, poll_id):
+#   poll = get_object_or_404(Poll, pk=poll_id)
+#   return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
   p = get_object_or_404(Poll, pk=poll_id)
